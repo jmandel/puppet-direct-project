2c595a1101f79fc6c2bff1594a8d2e41a489f6b7
Josh Mandel <joshua.mandel@childrens.harvard.edu>
2013-02-21 13:15:30 -0800

    Avoid namespace clobbering

--- modules/direct/files/config_client_py/add_dns.py
+++ modules/direct/files/config_client_py/add_dns.py
@@ -1,5 +1,5 @@
 from suds.client import Client
-from dns import A, MX
+from dns_entry import A, MX
 
 client = Client("http://localhost:8081/config-service/ConfigurationService?wsdl")
 
@@ -7,6 +7,7 @@ import sys
 record_type = sys.argv[1]
 
 dns_class = A if record_type =="A" else MX
-dns_entry = dns_class(sys.argv[2:])
+
+dns_entry = dns_class(*sys.argv[2:])
 
 dns_entry.add_to_config(client)
